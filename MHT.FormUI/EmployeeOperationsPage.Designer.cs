5b39c3e295ed64d8f071c90fb39298a1f6a59389
UtariD86 <69503659+UtariD86@users.noreply.github.com>
2023-05-16 16:15:40 +0300

    Autoscale none -utarid

--- MHT.FormUI/EmployeeOperationsPage.Designer.cs
+++ MHT.FormUI/EmployeeOperationsPage.Designer.cs
@@ -262,8 +262,7 @@
             // 
             // EmployeeOperationsPageUI
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.CornflowerBlue;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
